8c5c5269e6efbf2cfcb42cb17535590ee1a97826
sandeepgit3 <62005312+sandeepgit3@users.noreply.github.com>
2020-05-11 14:13:20 +0530

    Update resoucres.tf

--- resoucres.tf
+++ resoucres.tf
@@ -24,3 +24,17 @@ resource "aws_instance" "db" {
   }
 }
 
+resource "aws_s3_bucket" "sand123" {
+  bucket = "${var.tag_bucket_name}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = var.tag_bucket_name
+  }
+}
+
+
