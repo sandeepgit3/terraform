7755763884831552d4b854467965a2117ef00801
sandeepgit3 <62005312+sandeepgit3@users.noreply.github.com>
2020-05-11 10:55:49 +0530

    Update varaible.tf

--- varaible.tf
+++ varaible.tf
@@ -1,27 +1,24 @@
-varaible.tf
-+++++++++++++
-varaible "aws_region" {
- description = "Region for the VPC"
- default = "us-east-1"
- }
+variable "aws_region" {
+  description = "Region for the VPC"
+  default = "us-east-1"
+}
 
-varaible "vpc_cidr" {
- description = "CIDR for the VPC"
- default = "10.0.0.0/16"
- }
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.0.0.0/16"
+}
 
-varaible "public_subnet_cidr" {
- description = "CIDR for public subnet"
- default = "10.0.1.0/24"
- }
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.0.1.0/24"
+}
 
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
 
-varaible "private_subnet_cidr" {
- description = "CIDR for private subnet"
- default = "10.0.2.0/24
- }
- 
-variable "key_path" {
-  description = "SSH Public Key path"
-  default = "/home/core/.ssh/id_rsa.pub"
+variable "ami" {
+  description = "Amazon Linux AMI"
+  default = "ami-4fffc834"
 }
