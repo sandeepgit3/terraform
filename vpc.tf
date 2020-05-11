2b03929464468c70deab656ee590e8dbfb3d24b2
sandeepgit3 <62005312+sandeepgit3@users.noreply.github.com>
2020-05-11 10:58:13 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,85 +1,100 @@
+# Define our VPC
 resource "aws_vpc" "default" {
- cidr_block = "${var.vpc_cidr}"
- enable_dns_hostname = true
- 
-  tags { 
-   name = "dev_vpc"
-  }
- }
- 
-resource "aws_subnet" "public_subnet" {
- vpc_id = "${aws_vpc.default.id}"
- cidr_block = "${var.public_subnet_cidr}"
- availibility_zone = "us-west-1a"
- 
- tags { 
-  name = "public subnet"
-  }
- }
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
 
-resource "aws_subnet" "private_subnet" {
- vpc_id = "${aws_vpc.default.id}"
- cidr_block = "${var.public_subnet_cidr}"
- availibility_zone = "us-west-1b"
- 
- tags {
-  name = "private subnet"
+  tags = {
+    Name = "test-vpc"
   }
- }
-
-resource "aws_internet_gateway" "gw"
- vpc_id = "${aws_vpc.default.id}"
-
- tags {
-  Name= "VPC IGW"
-  }
- }
- 
-resource "aws_route_table" "web-public-rt" {
- vpc_id = "${aws_vpc.default.id}"
- 
- route{
-  cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.gw.id}"
-  }
- 
-tags {
- name = "Public Subnet RT"
- }
 }
 
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Web Public Subnet"
+  }
+}
+
+# Define the private subnet
+resource "aws_subnet" "private-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Database Private Subnet"
+  }
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags = {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
 resource "aws_route_table_association" "web-public-rt" {
   subnet_id = "${aws_subnet.public-subnet.id}"
   route_table_id = "${aws_route_table.web-public-rt.id}"
+}
 
-resource "aws_security_group" "sgweb"
- name = "vpc_test_web"
- description = " Allow incoming HTTP connections & ssh access"
- 
- ingress {
-  from_port = 80
-  to_port = 80
-  protocal = "tcp"
-  cidr_block = ["0.0.0.0/0"]
- }
- 
- ingress {
-  from_port = 443
-  to_port = 443
-  protocal = "tcp"
-  cidr_block = ["0.0.0.0/0"]
- }
- 
- ingress {
-  from_port = 22
-  to_port = 22
-  protocal = "tcp"
-  cidr_block = ["0.0.0.0/0"]
- }
- 
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
  vpc_id="${aws_vpc.default.id}"
 
-  tags {
+  tags = {
     Name = "Web Server SG"
   }
 }
@@ -112,7 +127,7 @@ resource "aws_security_group" "sgdb"{
 
   vpc_id = "${aws_vpc.default.id}"
 
-  tags {
+  tags = {
     Name = "DB SG"
   }
 }
