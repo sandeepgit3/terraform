947cd47c08d976813339cacadc7e6a6c300d7717
sandeepgit3 <62005312+sandeepgit3@users.noreply.github.com>
2020-05-12 11:12:34 +0530

    Update resoucres.tf

--- resoucres.tf
+++ resoucres.tf
@@ -37,4 +37,11 @@ resource "aws_s3_bucket" "sand123" {
   }
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "${aws_vpc.default.id}"
+  service_name = "com.amazonaws.us-east-1.s3"
 
+  tags = {
+    Environment = "test"
+ }
+}
